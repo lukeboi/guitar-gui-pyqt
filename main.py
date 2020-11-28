c5cc9a5fd5773ed4c797fc04a9b9836e6f42d70b
lukeboi <lukefarritor@gmail.com>
2020-11-27 18:10:07 -0600

    Working mvp

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import time
+
 import rtmidi
 from PyQt5.QtCore import QDir, QRect
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QKeySequence
+
+import midi_io
 
 
 class MainWindow(QMainWindow):
@@ -20,10 +24,10 @@ class MainWindow(QMainWindow):
 
 
 class midi_handling_bar(QWidget):
-    midiout = rtmidi.MidiOut()
-
     def __init__(self):
         QWidget.__init__(self)
+
+        midi_io.init()
 
         self.layout = QHBoxLayout()
         self.widget = QWidget()
@@ -35,19 +39,44 @@ class midi_handling_bar(QWidget):
 
         self.select_dropdown = QComboBox(self)
         self.select_dropdown.activated[str].connect(self.port_selected)
+        self.select_dropdown.setMaximumWidth(250)
+
+        self.play_button = QPushButton("Play Piece")
+        self.play_button.setMaximumWidth(130)
+        self.play_button.clicked.connect(self.refresh_button_pressed)
 
         layout.addWidget(self.refresh_button)
         layout.addWidget(self.select_dropdown)
+        layout.addWidget(self.play_button)
 
         self.refresh_button_pressed()
+        self.port_selected()
 
     def refresh_button_pressed(self):
-        available_ports = self.midiout.get_ports()
+        available_ports = midi_io.midiout.get_ports()
         self.select_dropdown.clear()
         self.select_dropdown.addItems(available_ports)
 
     def port_selected(self):
-        self.midiout.open_port(self.select_dropdown.currentIndex())
+        midi_io.midiout.open_port(self.select_dropdown.currentIndex())
+
+
+class file_speific_buttons(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+
+        self.layout = QHBoxLayout()
+        self.widget = QWidget()
+        self.widget.setLayout(self.layout)
+
+        self.save_button = QPushButton("Save File")
+        self.save_button.setMaximumWidth(130)
+        layout.addWidget(self.save_button)
+
+        self.save_as_button = QPushButton("Save File As")
+        self.save_as_button.setMaximumWidth(130)
+        layout.addWidget(self.save_as_button)
+
 
 
 class file_selection_list(QWidget):
@@ -59,12 +88,11 @@ class file_selection_list(QWidget):
 
         self.list = QTreeView()
 
-        file_path = None
+        self.current_file = None
         self.fileModel = QFileSystemModel()
-        # self.fileModel.setFilter(QDir.NoDotAndDotDot | QDir.Files)
+        self.fileModel.setFilter(QDir.NoDotAndDotDot | QDir.Files)
         self.list.setModel(self.fileModel)
         self.list.selectionModel().selectionChanged.connect(self.open_file)
-
 
         self.layout = QHBoxLayout()
         self.widget = QWidget()
@@ -79,23 +107,61 @@ class file_selection_list(QWidget):
             self.working_dir_path = path
 
     def open_file(self):
-        self.current_file = str(self.list.currentIndex())
-        print(self.current_file)
+        if text.document().isModified():
+            answer = QMessageBox.question(
+                window, None,
+                "You have unsaved changes. Save before opening another file?",
+                QMessageBox.Save | QMessageBox.Discard | QMessageBox.Cancel
+            )
+            if answer & QMessageBox.Save:
+                self.save()
+
+            elif answer & QMessageBox.Cancel:
+                return
+
+        self.current_file = self.working_dir_path + "/" + str(self.list.selectedIndexes()[0].data())
+        # self.list.itemFromIndex()
+        # print(self.current_file)
+        text.setPlainText(open(self.current_file).read())
 
     def save(self):
-        if file_path is None:
+        if self.current_file is None:
             self.save_as()
         else:
-            with open(file_path, "w") as f:
+            with open(self.current_file, "w") as f:
                 f.write(text.toPlainText())
             text.document().setModified(False)
 
     def save_as(self):
-        global file_path
         path = QFileDialog.getSaveFileName(window, "Save As")[0]
         if path:
-            file_path = path
+            self.current_file = path
             self.save()
+
+def play_piece(piece_str):
+    piece_commands = piece_str.splitlines()
+    for i in piece_commands:
+        print(i)
+        # remove comments
+        if i.find("#") != -1:
+            i = i[:i.find("#")]
+
+        # try to get command and corresponding arguments for command
+        command = i[:i.find(" ")]
+        args = i[i.find(" ") + 1:]
+
+        # if there is a command and args, run the command
+        if len(i) > 0 and i.find(" ") != -1 and len(command) > 0:
+
+            if command == "play":
+                note_nums = args.replace(" ", "").split(",")
+                for n in note_nums:
+                    print("playing note: " + str(n))
+                    midi_io.play_note(int(n))
+
+            elif command == "wait":
+                time.sleep(int(args))
+
 
 app = QApplication([])
 app.setApplicationName("Guitar Controller")
@@ -110,13 +176,20 @@ central = QWidget()
 central.setLayout(layout)
 
 midi_bar = midi_handling_bar()
+file_buttons = file_speific_buttons()
 file_list = file_selection_list()
 
 layout.addWidget(midi_bar, 0)
+# layout.addWidget(file_buttons, 0)
 layout.addWidget(file_list, 0)
 layout.addWidget(text, 0)
 window.setCentralWidget(central)
 window.resize(600, 450)
+
+file_buttons.save_button.clicked.connect(file_list.save)
+file_buttons.save_as_button.clicked.connect(file_list.save_as)
+
+midi_bar.play_button.clicked.connect(lambda: play_piece(str(text.toPlainText())))
 
 menu = window.menuBar().addMenu("&File")
 open_action = QAction("&Open Folder")
@@ -169,3 +242,4 @@ about_action.triggered.connect(show_about_dialog)
 
 window.show()
 app.exec_()
+midi_io.destroy_midi_out()
---
+++ midi_io.py
@@ -0,0 +1,15 @@
+import time
+import rtmidi
+
+midiout = midiout = rtmidi.MidiOut()
+
+def init():
+    midiout = rtmidi.MidiOut()
+
+
+def play_note(n):
+    note_on = [0x90, n, 100]  # channel 1, note n, velocity 100
+    midiout.send_message(note_on)
+
+def destroy_midi_out():
+    del midiout
